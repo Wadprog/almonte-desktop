6058e21183e738bf2ee2b80c13c792675ec26c47
Wadson Vaval <vavalson@hotmail.com>
2020-03-04 05:58:04 -0400

    added files from previous project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
+import Home from './pages/Home';
+import Clients from './pages/Clients';
+import Client from './pages/Client2';
+import Alert from './component/Alert';
+import Test from './component/Test';
+import Login from './pages/Login';
+import Loading from './component/layout/Loading';
+import Register from './pages/Register';
 
+import NewClient from './pages/NewClient';
+
+//Routing ..
+
+import { Switch, Route } from 'react-router-dom';
+import PrivateRoute from './component/PrivateRoute';
+
+//Redux ..
+
+import './App.css';
+import { loadProfiles } from './redux/actions/profile';
+import { loadLoans } from './redux/actions/loans';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './redux/actions/auth';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		store.dispatch(loadUser());
+		store.dispatch(loadProfiles());
+		store.dispatch(loadLoans());
+	}, []);
+
+	if (localStorage.token) setAuthToken(localStorage.token);
+	const { Fragment } = React;
+	return (
+		<Provider store={store}>
+			<Fragment>
+				<main>
+					<Navbar />
+					<div className=" mt-2">
+						<Alert />
+						<Switch>
+							<PrivateRoute exact path="/" component={Home} />
+							<PrivateRoute exact path="/clients" component={Clients} />
+							<PrivateRoute path="/client/:id" component={Client} />
+							<Route path="/login" component={Login} />
+							<Route path="/load" component={Loading} />
+							<Route path="/newClient" component={NewClient} />
+							<PrivateRoute path="/register" component={Register} />
+							<PrivateRoute path="/test" component={Test} />
+						</Switch>
+					</div>
+					<Footer />
+				</main>
+			</Fragment>
+		</Provider>
+	);
 }
 
 export default App;
